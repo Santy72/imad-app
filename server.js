64a0eee26817227b80faf440fd817b2f0645af1e
Santy72 <31274439+Santy72@users.noreply.github.com>
2017-08-28 15:06:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/article-one',function (req ,res) {
+    res.send('You asked for article one and here it is !! ');
+});
+
+app.get('/ui/article-two',function (req ,res) {
+    res.send('You asked for article two and there it is !! ');
+});
+
+app.get('/ui/article-third',function (req ,res) {
+    res.send('You asked for article one and take it !! ');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
